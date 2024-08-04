c77a396773ca74a5365b2ae1de122f1877a94616
Adyatma Fulvian Rooshardiyanto <adyatma.fulvian@alpabit.com>
2024-08-05 00:34:57 +0700

    Add login feature

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,1 +1,20 @@
 import apiService from './api/apiService.js';
+
+$(document).ready(() => {
+    $('#login-form').on('submit', (event) => {
+        event.preventDefault();
+
+        const bodyRequest = {
+            username: $('#username').val(),
+            password: $('#password').val()
+        }
+
+        apiService.post('api/auth/login', bodyRequest)
+            .done(response => {
+                alert('Login successfully:', response);
+            })
+            .fail((jqXHR, textStatus, errorThrown) => {
+                alert('Error Login:', textStatus, errorThrown);
+            });
+    });
+});
