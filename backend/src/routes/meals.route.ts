d2563f7eb88a165a69daaef58095a2a051af8615
Rakemoon <rakemoonkuzmin@gmail.com>
2024-08-04 22:54:21 +0700

    feat: Meals get bookmarked

--- backend/src/routes/meals.route.ts
+++ backend/src/routes/meals.route.ts
@@ -1,11 +1,12 @@
 
 import { Hono } from 'hono';
-import { findMealByExternalId, getMealIngeredientsByMealId } from '../controllers/meals.controller';
+import { findMealByExternalId, getMealIngeredientsByMealId, findMealBookmark } from '../controllers/meals.controller';
 import { authentication, authenticationUser } from '../middleware/auth.middleware';
 
 const app = new Hono();
 
 app.get('/:externalId', authentication, authenticationUser, findMealByExternalId);
 app.get('/:mealId/ingredients', authentication, authenticationUser, getMealIngeredientsByMealId);
+app.get('/:externalId/users', authentication, authenticationUser, findMealBookmark);
 
 export default app;
