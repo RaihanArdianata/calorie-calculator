ec7eb6acd452a4b20cf554cbf0b2f259c953b593
Rakemoon <rakemoonkuzmin@gmail.com>
2024-08-04 21:10:30 +0700

    feat: users crud

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,10 +2,12 @@ import { Hono } from 'hono';
 
 import auth from './auth.route';
 import meals from './meals.route';
+import users from './users.route';
 
 const app = new Hono();
 
 app.route('/auth', auth);
 app.route('/meals', meals);
+app.route('/users', users);
 
 export default app;
---
+++ backend/src/services/users.service.ts
@@ -0,0 +1,43 @@
+import { isNaN } from "lodash";
+import { prisma } from "../bin/database";
+import { transformPhoneNumber } from "../utils/formater";
+import { bcryptHash } from "../utils/hashing";
+import { FetchAllSchemaType, CreateSchemaType, DeleteSchemaType } from "../utils/validator/users.validator";
+
+export const fetchAll = async ({ page, limit }: FetchAllSchemaType) => {
+  page = isNaN(page) ? 1 : +page;
+  limit = isNaN(limit) ? 10 : +limit;
+  const skip = (page - 1) * limit;
+  return prisma.users.findMany({
+    skip,
+    take: limit,
+  });
+}
+
+export const fetchDatabaseSize = async() => prisma.users.count();
+
+export const create = async (data: CreateSchemaType) => 
+  prisma.users.upsert({
+    create: {
+      email: data.email,
+      first_name: data.first_name,
+      last_name: data.last_name,
+      password: await bcryptHash(data.password),
+      phone: await transformPhoneNumber(data.phone),
+      username: data.username,
+      role_id: data.role_id,
+    },
+    update: {
+      ...data
+    },
+    where: {
+      id: data.id
+    }
+  });
+
+
+export const removes = async ({ ids }: DeleteSchemaType) => 
+  prisma.users.deleteMany({ where: { id: { in: ids } } });
+
+export const favMeals = async (id: string) =>
+  prisma.favorite_meals.findMany({ where: { user_id: id }});
