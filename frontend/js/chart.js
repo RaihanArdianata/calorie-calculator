a549268fd0a7561e8a599dba4c2f8a3f013dc228
RaihanArdianata <raihan.ardianata@gmail.com>
2024-08-05 15:54:05 +0700

    add section hero,calories,agenda, and foods

---
+++ frontend/js/chart.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  const ctx = document.getElementById('chart');
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [
+        {
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  });
+});
