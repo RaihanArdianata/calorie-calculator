1847301f2d58eba553bee41593244be70caeb6e0
RaihanArdianata <raihan.ardianata@gmail.com>
2024-08-06 12:45:05 +0700

    change hash from bun to bcrypy

--- backend/src/utils/hashing.ts
+++ backend/src/utils/hashing.ts
@@ -1,16 +1,18 @@
-import { password as bunHash } from "bun";
+import * as bcrypt from 'bcrypt';
 
-// export const bcryptHash = async (password: string) => await Bun.password.hash(password, {
+export const bcryptHash = async (password: string) => {
+  const saltRounds = 10;
+  return await bcrypt.hash(password, saltRounds);
+};
+
+export const bcryptVerify = async (password: string, hashedPassword: string) => {
+  return await bcrypt.compare(password, hashedPassword);
+};
+
+
+// export const bcryptHash = async (password: string) => await bunHash.hash(password, {
 //   algorithm: "bcrypt",
 //   cost: 4,
 // });
 
-// export const bcryptVerify = async (password: string, hashedPassword: string) => await Bun.password.verify(password, hashedPassword, "bcrypt");
-
-
-export const bcryptHash = async (password: string) => await bunHash.hash(password, {
-  algorithm: "bcrypt",
-  cost: 4,
-});
-
-export const bcryptVerify = async (password: string, hashedPassword: string) => await bunHash.verify(password, hashedPassword, "bcrypt");
+// export const bcryptVerify = async (password: string, hashedPassword: string) => await bunHash.verify(password, hashedPassword, "bcrypt");
