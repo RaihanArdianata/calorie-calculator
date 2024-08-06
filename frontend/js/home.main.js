03d7121480f0b3e26f5dd2707027b1d7985b3c70
RaihanArdianata <raihan.ardianata@gmail.com>
2024-08-06 12:26:56 +0700

    home add list diet record

--- frontend/js/home.main.js
+++ frontend/js/home.main.js
@@ -1,8 +1,42 @@
 import externalApiService from './api/externalApiService.js';
 
 $(document).ready(() => {
+  // state
+  const agenda = ['BREAKFAST', 'LUNCH', 'DINNER', 'SNACK'];
+
+  // selector
   const mealsContainer = $('#meals');
   const categoriesContainer = $('#categories');
+  const agendaContainer = $('#agenda');
+
+  const generateAgenda = () => {
+    agenda.forEach((item) => {
+      agendaContainer.append(`
+          <div class="card">
+            <header class="card-header">
+              <p class="card-header-title">${item?.toLowerCase()}</p>
+              <button class="card-header-icon" aria-label="more options">
+                <span class="icon">
+                  <i class="fas fa-angle-down" aria-hidden="true"></i>
+                </span>
+              </button>
+            </header>
+            <div class="card-content">
+              <div class="content">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis
+                mauris.
+                <a href="#">@bulmaio</a>. <a href="#">#css</a> <a href="#">#responsive</a>
+                <br />
+                <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+              </div>
+            </div>
+            <footer class="card-footer">
+              <a href="#" class="card-footer-item">View</a>
+            </footer>
+          </div>  
+        `);
+    });
+  };
 
   const fetchCategories = () => {
     externalApiService
@@ -75,6 +109,7 @@ $(document).ready(() => {
     searchMeals(bodyRequest.query);
   });
 
-  // fetch first
+  // generate first
   fetchCategories();
+  generateAgenda();
 });
